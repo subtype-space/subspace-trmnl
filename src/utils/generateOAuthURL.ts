1d0b1236c142230b2245efd67f2e4f5e2e312834
subtype <andrew.subowo@gmail.com>
2025-12-17 19:45:55 -0500

    oauth implementation

--- src/utils/generateOAuthURL.ts
+++ src/utils/generateOAuthURL.ts
@@ -1,23 +1,20 @@
-import { logger } from "./logger.js";
-
+import { logger } from './logger.js'
 export function createOAuthURLs() {
-    const authBaseURL = new URL(
-        `http://${process.env.AUTH_SERVER_URL}/realms/${process.env.AUTH_REALM}/`
-    )
+  const authServer = process.env.AUTH_SERVER_URL
+  const realm = process.env.AUTH_REALM
 
-    return {
-        issuer: authBaseURL.toString(),
-        introspection_endpoint: new URL(
-            "protocol/openid-connect/token/introspect",
-            authBaseURL,
-        ).toString(),
-        authorization_endpoint: new URL(
-            "protocol/openid-connect/auth",
-            authBaseURL,
-        ).toString(),
-        token_endpoint: new URL(
-            "protocol/openid-connect/token",
-            authBaseURL,
-        ).toString()
-    }
+  if (!authServer || !realm) {
+    logger.error('[AUTH] ENV VAR AUTH_SERVER_URL or AUTH_REALM not set. No auth possible.')
+    return null
+  }
+
+  const realmBase = new URL(`/realms/${realm}`, authServer)
+  const issuer = realmBase.toString().replace(/\/$/, '')
+
+  return {
+    issuer: issuer,
+    introspection_endpoint: new URL('protocol/openid-connect/token/introspect', realmBase).toString(),
+    authorization_endpoint: new URL('protocol/openid-connect/auth', realmBase).toString(),
+    token_endpoint: new URL('protocol/openid-connect/token', realmBase).toString(),
+  }
 }
