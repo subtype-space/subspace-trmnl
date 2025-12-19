c95efc4f6220fa5bf4c31e0ce040c19a2d94a471
asubowo <andrew.subowo@gmail.com>
2025-12-19 13:22:45 -0500

    updates

--- src/v1/routers/trmnlRouter.ts
+++ src/v1/routers/trmnlRouter.ts
@@ -8,9 +8,12 @@ import { trmnlUninstallController } from '../controllers/trmnlUninstallControlle
 const router = express.Router()
 
 // Should be mounted at /trmnl, so <api endpoint>/v1/trmnl/install and etc.
-router.get('/install', trmnlInstallController)
+router.post('/install', trmnlInstallController)
 router.post('/install_success', requireTrmnlAuth, trmnlInstallSuccessController)
 router.post('/markup', express.urlencoded({ extended: false }), requireTrmnlAuth, trmnlMarkupController)
 router.post('/uninstall', requireTrmnlAuth, trmnlUninstallController)
+
+// TODO - implement /settings endpoint
+// router.
 
 export default router
